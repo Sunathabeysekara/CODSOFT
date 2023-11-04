3bfcf794392ce17107c5763cf82a7f9df8a9ff44
Sunath Abeysekara <abeysekarasunath666@gmail.com>
2023-11-04 18:34:11 +0530

    update with score
    random number guess with user's score

--- RandomNumber/src/Randomn.java
+++ RandomNumber/src/Randomn.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 public class Randomn {
 	public static Scanner sc=new Scanner(System.in);
 	
-	public static boolean check(int num,int guess) {//get input & random value
+	public static boolean check(int num,int guess,int score) {//get input & random value
 		boolean bool=false;//intialize to getut put
 		if(guess!=num) {
 			if(guess<num) {
 			System.out.println("too low!.");//w	henguess is low	
 			bool=false;
+			
 			}else {
-				System.out.println("too high!");//	when guess is high
+				System.out.println("too high!.");//	when guess is high
 				bool=false;
 			}
 		}else {
-			System.out.println("correct guess!.");///when correct guess
+			System.out.println("correct guess!.\n Your score: "+score);///when correct guess
 			bool=true;
 		}
 		return bool;		
 	}
 	public static void ans(int i,int number) {
+		int score=3;//maximum score reducing with number of guesses
 		while(i<=3) {
 			System.out.println("Geuess the number:");
 			int guess=sc.nextInt();
-			if(check(number,guess)==true) {
+			if(check(number,guess,score--)==true) {//score reduce with guess
 				break;//break when win
 			}else if(i==3){//to lose output
-				System.out.println("you lose!.Max time reached.");
+				System.out.println("you lose!.Max time reached.\nScore: "+score);
 			}	
 			i++;
 		}
@@ -39,7 +41,7 @@ public class Randomn {
 		
 		int number=r.nextInt(100);	
 		int i=1;
-		//System.out.println(number);
+		System.out.println(number+1);
 		ans(i,number+1);
 		sc.close();//close scanne class
 	}
